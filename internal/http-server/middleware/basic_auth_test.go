c81f34256a7169c2ec37301fafec5515da964a21
y.anikin <y.anikin@rkeeper.ru>
2026-09-04 21:52:10 +0500

    тесты на авторизацию

---
+++ internal/http-server/middleware/basic_auth_test.go
@@ -0,0 +1,65 @@
+package middleware
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/joho/godotenv"
+)
+
+func TestBasicAuth_Unauthorized(t *testing.T) {
+	username := "test"
+	password := "test"
+
+	handler := BasicAuth(username, password)(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		}),
+	)
+
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec := httptest.NewRecorder()
+
+	handler.ServeHTTP(rec, req)
+
+	if rec.Code != http.StatusUnauthorized {
+		t.Fatalf(
+			"expected status %d, got %d",
+			http.StatusUnauthorized,
+			rec.Code,
+		)
+	}
+}
+
+func TestBasicAuth_Success(t *testing.T) {
+	if err := godotenv.Load("../../../.env"); err != nil {
+		t.Fatal("failed to load .env")
+	}
+
+	username := os.Getenv("HTTP_SERVER_USER")
+	password := os.Getenv("HTTP_SERVER_PASSWORD")
+
+	handler := BasicAuth(username, password)(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		}),
+	)
+
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+
+	req.SetBasicAuth(username, password)
+
+	rec := httptest.NewRecorder()
+
+	handler.ServeHTTP(rec, req)
+
+	if rec.Code != http.StatusOK {
+		t.Fatalf(
+			"expected status %d, got %d",
+			http.StatusOK,
+			rec.Code,
+		)
+	}
+}
